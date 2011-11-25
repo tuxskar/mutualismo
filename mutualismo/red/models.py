9f64b1adc46c9761bbddac8b93ffb883a36fb0ff
Alejandro Gómez <alejandroogomez@gmail.com>
2011-11-26 00:40:46 +0100

    First draft of the models
    I've written the first draft of the models of ``red`` app for
    further discussion and improvement.
    
    Category and SubCategory
      I've defined SubCategory using Category as a base class, since
      they share some attributes. We should define a limit when it
      comes to create subcategories.
    
    Trade
      The base class of every object and service. I haven't added yet
      the tag field since we need to discuss what tagging 3rd party
      app are we going to use.
    
    Good
      I've added the state information to to this class (a base class
      for every good).
    
    Loan
      A good that is loaned for a certain period.
    
    Gift
      A good that is offered as a gift. I've included a boolean field
      to distinguish the communal goods, which have to stay in the
      network.
    
    Service
      I've added fields for specifying the period in which the service
      is available. The ``availability`` field is for adding some notes
      about when exactly is the service available (e.g. sunday morning).
    
    Exchange
      Represents and exchange, it only includes the trade and date since
      its related to the giver (trade object's owner) and the receiver
      (m2m relationship). We probably have to change this because is
      not very obvious and the relations are messy.
    
    User
      We store the offerings and demands that he/she has, a history with
      all its transactions and an optional string to specify his/her
      location.

--- mutualismo/red/models.py
+++ mutualismo/red/models.py
@@ -1,3 +1,73 @@
+from django.contrib.auth.models import User as BaseUser
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name        = models.CharField(max_length=124)
+    description = models.TextField()
+
+class SubCategory(Category):
+    parent = models.ForeignKey(Category, related_name='sub')
+
+
+class Trade(models.Model):
+    """
+    Represents an object or action which can be part of a trade within our network. 
+    """
+    name        = models.CharField(max_length=124)
+    description = models.TextField()
+    category    = models.ForeignKey(Category)
+    # TODO tags
+
+
+class Good(Trade):
+    """
+    Represents a good that can be offered or demanded. 
+    """
+    # XXX should be mandatory for offers, N/A for demands
+    #STATE = ('')
+    pass
+
+
+class Loan(Good):
+    """
+    Represents a good that is available for loan.
+    """
+    # TODO we are discussing this
+    max_time = models.CharField(max_length=140)
+
+
+class Gift(Good):
+    """
+    Represents a good that is gifted by his/her owner.
+    """
+    communal = models.BooleanField()
+
+
+class Service(Trade):
+    """
+    Represents a service that can be offered or demanded.  
+    """
+    starts       = models.DateTimeField()
+    ends         = models.DateTimeField()
+    availability = models.TextField()
+
+
+class Exchange(models.Model):
+    """
+    Represents an exchange between two users.
+    """
+    #CHOICE (give, receive)
+    trade    = models.ForeignKey(Trade)
+    date     = models.DateTimeField()
+
+
+class User(BaseUser):
+    """
+    Extends the ``django.contrib.auth.User`` class to store offers, demands, a
+    history of the exchanges and more suitable information.
+    """
+    offerings = models.ManyToManyField(Trade, related_name='offer')
+    demands   = models.ManyToManyField(Trade, related_name='demand')
+    history   = models.ManyToManyField(Exchange)
+    location  = models.CharField(max_length=124)
