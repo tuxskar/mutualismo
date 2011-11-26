b7525aec564fc27dd49406ee7d7126dcbfc01a54
Alejandro Gómez <alejandroogomez@gmail.com>
2011-11-27 00:29:35 +0100

    Tests to fill with the model specs

--- mutualismo/red/tests/test_models.py
+++ mutualismo/red/tests/test_models.py
@@ -1,1 +1,37 @@
 from django.test import TestCase
+
+
+class CategoryTest(TestCase):
+    pass
+
+
+class SubcategoryTest(TestCase):
+    pass
+
+
+class TradeTest(TestCase):
+    pass
+
+
+class GoodTest(TestCase):
+    pass
+
+
+class LoanTest(TestCase):
+    pass
+
+
+class GiftTest(TestCase):
+    pass
+
+
+class SeriveTest(TestCase):
+    pass
+
+
+class UserTest(TestCase):
+    pass
+
+
+class ExchangeTest(TestCase):
+    pass
