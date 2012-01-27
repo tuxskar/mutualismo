c2901fef7e03e424ee0138b73bce1b78c666a7c1
Alejandro Gómez <alejandroogomez@gmail.com>
2012-01-27 19:22:00 +0100

    models in the admin interface

---
+++ mutualismo/red/admin.py
@@ -0,0 +1,32 @@
+from django.contrib import admin
+from red.models import Loan, Gift, Service, Demand, User, Exchange
+
+class LoanAdmin(admin.ModelAdmin):
+    pass
+
+
+class GiftAdmin(admin.ModelAdmin):
+    pass
+
+
+class ServiceAdmin(admin.ModelAdmin):
+    pass
+
+
+class DemandAdmin(admin.ModelAdmin):
+    pass
+
+class UserAdmin(admin.ModelAdmin):
+    pass
+
+
+class ExchangeAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Loan, LoanAdmin)
+admin.site.register(Gift, GiftAdmin)
+admin.site.register(Service, ServiceAdmin)
+admin.site.register(Demand, DemandAdmin)
+admin.site.register(User, UserAdmin)
+admin.site.register(Exchange, ExchangeAdmin)
