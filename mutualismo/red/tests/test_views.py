4903ab21c65f589f885b89f7ed3e2b39583afd17
Alejandro Gómez <alejandroogomez@gmail.com>
2012-01-30 23:56:42 +0100

    test for index view

--- mutualismo/red/tests/test_views.py
+++ mutualismo/red/tests/test_views.py
@@ -1,5 +1,14 @@
-from django.test import TestCase
+from django.test import Client, TestCase
 
-# TODO
-# we should test:
-#  - which templates have been rendered
+
+class TestIndex(ViewTestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_templates(self):
+        urls = ['', '/']
+        responses = [self.client.get(url) for url in urls]
+        templates = ['base.html', 'index.html']
+        for response in responses:
+            for template in templates:
+                self.assertTemplateUsed(response, template)
