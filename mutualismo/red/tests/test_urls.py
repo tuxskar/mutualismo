b585a6eff7b0674d633893a61db1e434d456165e
Alejandro Gómez <alejandroogomez@gmail.com>
2012-01-30 23:47:19 +0100

    index url and view

--- mutualismo/red/tests/test_urls.py
+++ mutualismo/red/tests/test_urls.py
@@ -1,3 +1,20 @@
-# TODO
-# we should test:
-#  - HTTP codes
+from django.test import Client, TestCase
+
+
+class UrlTestCase(TestCase):
+    """Helper class for testing URLs."""
+    def setUp(self):
+        self.client = Client()
+
+    def assertHttpOk(self, url):
+        """Checks that the given URL returns a 200 HTTP code."""
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+
+class TestURLs(UrlTestCase):
+    def test_index_ok(self):
+        urls = ['', '/']
+        for url in urls:
+            self.assertHttpOk(url)
+
--- mutualismo/urls.py
+++ mutualismo/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls.defaults import patterns, include, url
 
-# Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
+
+from red.views import index
 
 urlpatterns = patterns('',
     # Examples:
@@ -11,6 +12,6 @@ urlpatterns = patterns('',
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$',      index),
 )
