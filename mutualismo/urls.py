c439d0b422613548c1a436aaf0d7461f8e0b42a3
Alejandro Gómez <alejandroogomez@gmail.com>
2012-02-03 17:09:16 +0100

    faq first draft

--- mutualismo/urls.py
+++ mutualismo/urls.py
@@ -5,5 +5,6 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
+    url('^faq/',    include('faq.urls')),
     url(r'^red/',   include('red.urls')),
 )
