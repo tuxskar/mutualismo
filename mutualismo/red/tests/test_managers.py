a8b3a6659a49c240c62eba9fb9eccacf6dca9435
Alejandro Gómez <alejandroogomez@gmail.com>
2012-02-10 21:06:53 +0100

    test the order of the ``TradeManager`` queries

--- mutualismo/red/tests/test_managers.py
+++ mutualismo/red/tests/test_managers.py
@@ -50,14 +50,44 @@ class TradeManagerTest(TestCase):
         for offer in offers:
             self.assertEqual(offer.owner, alice)
 
+    def test_user_offers_sorted_by_date(self):
+        # newest comes first
+        alice = User.objects.get(username=u'Alice')
+        offers = self.manager.offers(alice)
+        sorted_offers = sorted(list(offers),
+                               key=lambda offer: offer.date,
+                               reverse=True)
+        for index, offer in enumerate(sorted_offers):
+            self.assertEqual(offer, offers[index])
+
     def test_user_demands(self):
         alice = User.objects.get(username=u'Alice')
         demands = self.manager.demands(alice)
         for demand in demands:
             self.assertEqual(demand.owner, alice)
 
+    def test_user_demands_sorted_by_date(self):
+        # newest comes first
+        alice = User.objects.get(username=u'Alice')
+        demands = self.manager.demands(alice)
+        sorted_demands = sorted(list(demands),
+                               key=lambda demand: demand.date,
+                               reverse=True)
+        for index, demand in enumerate(sorted_demands):
+            self.assertEqual(demand, demands[index])
+
     def test_user_services(self):
         alice = User.objects.get(username=u'Alice')
         services = self.manager.services(alice)
         for service in services:
             self.assertEqual(service.owner, alice)
+
+    def test_user_services_sorted_by_date(self):
+        # newest comes first
+        alice = User.objects.get(username=u'Alice')
+        services = self.manager.services(alice)
+        sorted_services = sorted(list(services),
+                               key=lambda service: service.date,
+                               reverse=True)
+        for index, service in enumerate(sorted_services):
+            self.assertEqual(service, services[index])
