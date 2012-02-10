42189045bef1e09e875169a6d4278d3076d9bd5f
Alejandro Gómez <alejandroogomez@gmail.com>
2012-02-10 14:41:32 +0100

    serving of static media from subdirectories

--- mutualismo/urls.py
+++ mutualismo/urls.py
@@ -6,6 +6,14 @@ admin.autodiscover()
 urlpatterns = patterns('',
     url(r'^admin/',         include(admin.site.urls)),
     url(r'^accounts/',      include('registration.backends.default.urls')),
-    url('^faq/',            include('faq.urls')),
+    url(r'^faq/',           include('faq.urls')),
     url(r'',                include('red.urls')),
 )
+
+import settings
+if settings.DEBUG:
+    urlpatterns += patterns('',
+        # FIXME: if we put ``static`` here it does not serve static media
+        (r'^statics/(?P<path>.*)$', 'django.views.static.serve', 
+            {'document_root': settings.STATIC_ROOT, 'show_indexes': True},),
+    )
