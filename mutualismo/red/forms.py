d38fd8eda5b60082fe6b3d4028f17673b2fef512
oscar <tuxskar@gmail.com>
2012-02-10 11:50:44 +0100

    forgot label

--- mutualismo/red/forms.py
+++ mutualismo/red/forms.py
@@ -5,5 +5,5 @@ class ContactForm(forms.Form):
     """Form for submitting a message to the site administrators."""
     subject = forms.CharField(label=_("Subject"), max_length=100)
     message = forms.CharField(label=_("Message"))
-    sender = forms.EmailField()
+    sender = forms.EmailField(label=_("Sender"))
     cc_myself = forms.BooleanField(label=_("Send me a copy"), required=False)
