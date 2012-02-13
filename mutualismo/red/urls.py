3da9bdaafec70f899c63b49ca005e908f3365bee
oscar <tuxskar@gmail.com>
2012-02-13 01:01:21 +0100

    all urls named¡

--- mutualismo/red/urls.py
+++ mutualismo/red/urls.py
@@ -6,16 +6,16 @@ urlpatterns = patterns('red.views',
     url(r'^$',                                     index,),
     url(r'^about',                                 about, name='about'),
     url(r'^contact',                               contact, name='contact'),
-    url(r'^dashboard',                             dashboard),
-    url(r'^offer/(?P<offer_slug>[\w-]+)',          offer),
-    url(r'^demand/(?P<demand_slug>[\w-]+)',        demand),
-    url(r'^create/demand',                         create_demand),
-    url(r'^delete/offer/(?P<offer_slug>[\w-]+)',   delete_offer),
-    url(r'^delete/demand/(?P<demand_slug>[\w-]+)', delete_demand),
+    url(r'^dashboard',                             dashboard, name='dashboard'),
+    url(r'^offer/(?P<offer_slug>[\w-]+)',          offer, name='offer'),
+    url(r'^demand/(?P<demand_slug>[\w-]+)',        demand, name='demand'),
+    url(r'^create/demand',                         create_demand, name='new_demand'),
+    url(r'^delete/offer/(?P<offer_slug>[\w-]+)',   delete_offer, name='delete_offer'),
+    url(r'^delete/demand/(?P<demand_slug>[\w-]+)', delete_demand, name='delete_demand'),
 )
 
 import settings
 if settings.DEBUG:
     urlpatterns += patterns('',
-        (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
+        (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': setings.STATIC_ROOT}),
     )
